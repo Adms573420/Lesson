45138effa211532e4e797ac1ff7d271f404357dc
Adem OKUR <adem.okur5734@gmail.com>
2021-02-27 12:19:37 +0200

    version 1.1

---
+++ src/Lesson_7Homework.java
@@ -0,0 +1,128 @@
+import com.sun.tools.javac.Main;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Lesson_7Homework {
+    /* public static void main(String[] args) {
+         System.out.println("Gun Adını Yazınız:");
+         Scanner scanner=new Scanner(System.in);
+         String gun= scanner.next();
+
+
+
+        String str = gun.toLowerCase(Locale.ROOT);
+
+        switch (gun){
+             case "Cuma":
+                 System.out.println("Müslümanlar için kutsal gün.");
+                 break;
+             case "Cumaresi":
+                 System.out.println("Yahudiler için kutsal gün.");
+                 break;
+             case "Pazar":
+                 System.out.println("Hristiyanlar için kutsal gün.");
+                 break;
+             default:
+                 System.out.println("Kutsal gün degil.");
+
+         }
+     }
+    public static void main(String[] args) {
+        System.out.println("Bir harf yazın.");
+        Scanner scanner=new Scanner(System.in);
+        String harf=scanner.nextLine();
+
+
+        String str=harf.toLowerCase(Locale.ROOT);
+        switch (harf){
+            case"a":
+            case"e":
+            case"i":
+            case"o":
+            case"u":
+                System.out.println("Sesli harf");
+                break;
+            case"b":
+            case"c":
+            case"d":
+            case"f":
+                System.out.println("Sessiz harf");
+                break;
+            default:
+                System.out.println("Geçersiz Karakter.");
+
+        }
+    }
+    public static void main(String[] args) {
+        System.out.println("Bir tamsayı yazın:");
+        Scanner scanner = new Scanner(System.in);
+        int tamsayı = scanner.nextInt();
+
+        switch (tamsayı) {
+            case 9:
+                System.out.println("Bir basamaklı en büyük tam sayı");
+                break;
+            case 99:
+                System.out.println("İki basamaklı en büyük tam sayı");
+                break;
+            case 999:
+                System.out.println("Üç basamaklı en büyük tam sayı");
+                break;
+            default:
+                System.out.println("Yorum yok.");
+        }
+
+
+    }
+    public static void main(String[] args) {
+        System.out.println("Bir ay ismi yazınız.");
+        Scanner scanner=new Scanner(System.in);
+        String ay=scanner.nextLine();
+
+
+        switch (ay){
+            case"Ocak":
+            case"Temmuz":
+            case"Mart":
+            case"Mayıs":
+            case"Agustos":
+            case"Ekim":
+            case"Aralık":
+                System.out.println("Bu ay 31 gündür.");
+                break;
+            case"Subat":
+                System.out.println("Bu ay Subat ayıdır.");
+                break;
+            case"Nisan":
+            case"Haziran":
+            case"Kasım":
+            case"Eylül":
+                System.out.println("Bu ay 30 gündür.");
+                break;
+            default:
+                System.out.println("Gecersiz ay ismi.");
+        }
+    }
+    public static void main(String[] args) {
+        String harf="A,B,C,D";
+        System.out.println("Bir harf yaınız:");
+        Scanner scanner=new Scanner(System.in);
+        harf=scanner.nextLine();
+
+        switch (harf){
+            case"A":
+            case"B":
+            case"D":
+                System.out.println("Yanlıs Cevap");
+                break;
+            case"C":
+                System.out.println("Dogru Cevap");
+                break;
+            default:
+                System.out.println("Gecersiz cevap sıkkı");
+        }
+    }*/
+
+
+}
